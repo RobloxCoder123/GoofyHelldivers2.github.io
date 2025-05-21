8b23a6684a2b4bca005bbd4794d00343c659793f
RobloxCoder123 <pytlicekD02@students.manateeschools.net>
2025-05-21 16:52:35 +0000

    Stuff

---
+++ TestRequest.py
@@ -0,0 +1,8 @@
+import requests
+
+#url = "https://helldiverstrainingmanual.com/api/v1/war/major-orders" #<----- Reactivate when ready
+'''response = requests.get(url)
+if response.status_code == 200:
+    print("Success:", response.json())
+else:
+    print("Error:", response.status_code)'''
